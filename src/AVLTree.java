70694d169f99b03a089c1117a83d884ac52c6fd7
Aiganymus <aiganym7777@gmail.com>
2017-12-20 18:29:05 +0600

    message

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -10,45 +10,36 @@ class AVLTree {
     Node rightRotate(Node y) {
         Node x = y.left;
         Node T2 = x.right;
- 
         x.right = y;
-        y.left = T2;
- 
+        y.left = T2; 
         y.height = Math.max(height(y.left), height(y.right)) + 1;
         x.height = Math.max(height(x.left), height(x.right)) + 1;
- 
         return x;
     }
  
     Node leftRotate(Node x) {
         Node y = x.right;
         Node T2 = y.left;
- 
         y.left = x;
         x.right = T2;
- 
         x.height = Math.max(height(x.left), height(x.right)) + 1;
         y.height = Math.max(height(y.left), height(y.right)) + 1;
- 
         return y;
     }
  
     int getBalance(Node N) {
-        if (N == null)
-            return 0;
+        if (N == null) return 0;
         return height(N.left) - height(N.right);
     }
  
     Node insert(Node node, int key) {
-        if (node == null)
-            return (new Node(key));
+        if (node == null) return (new Node(key));
  
         if (key < node.key)
             node.left = insert(node.left, key);
         else if (key > node.key)
             node.right = insert(node.right, key);
-        else 
-            return node;
+        else return node;
  
         node.height = 1 + Math.max(height(node.left),
                               height(node.right));
@@ -74,24 +65,22 @@ class AVLTree {
         return node;
     }
  
-    Node minValueNode(Node node) {
+    Node minNode(Node node) {
         Node current = node;
-
         while (current.left != null)
            current = current.left;
- 
         return current;
     }
  
-    Node deleteNode(Node root, int key) {
+    Node delete(Node root, int key) {
         if (root == null)
             return root;
-
+        
         if (key < root.key)
-            root.left = deleteNode(root.left, key);
+            root.left = delete(root.left, key);
  
         else if (key > root.key)
-            root.right = deleteNode(root.right, key);
+            root.right = delete(root.right, key);
 
         else {
             if ((root.left == null) || (root.right == null)) {
@@ -109,11 +98,9 @@ class AVLTree {
                     root = temp; 
             }
             else {
-                Node temp = minValueNode(root.right);
- 
+                Node temp = minNode(root.right);
                 root.key = temp.key;
-
-                root.right = deleteNode(root.right, temp.key);
+                root.right = delete(root.right, temp.key);
             }
         }
  
@@ -143,11 +130,11 @@ class AVLTree {
         return root;
     }
  
-    void preOrder(Node node) {
+    void print(Node node) {
         if (node != null) {
             System.out.print(node.key + " ");
-            preOrder(node.left);
-            preOrder(node.right);
+            print(node.left);
+            print(node.right);
         }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,22 +1,18 @@
 
 public class Test {
 	 public static void main(String[] args) {
-        AVLTree tree = new AVLTree();
- 
-        tree.root = tree.insert(tree.root, 9);
-        tree.root = tree.insert(tree.root, 5);
-        tree.root = tree.insert(tree.root, 10);
-        tree.root = tree.insert(tree.root, 0);
-        tree.root = tree.insert(tree.root, 6);
-        tree.root = tree.insert(tree.root, 11);
-        tree.root = tree.insert(tree.root, -1);
+        AVLTree tree = new AVLTree(); 
         tree.root = tree.insert(tree.root, 1);
         tree.root = tree.insert(tree.root, 2);
- 
-        tree.preOrder(tree.root);
- 
-        tree.root = tree.deleteNode(tree.root, 10);
- 
-        tree.preOrder(tree.root);
+        tree.root = tree.insert(tree.root, 1);
+        tree.root = tree.insert(tree.root, 4);
+        tree.root = tree.insert(tree.root, 12);
+        tree.root = tree.insert(tree.root, 5);
+        tree.root = tree.insert(tree.root, 0);
+        tree.root = tree.insert(tree.root, 56);
+        tree.root = tree.insert(tree.root, 78); 
+        tree.print(tree.root); 
+        tree.root = tree.delete(tree.root, 10); 
+        tree.print(tree.root);
 	 }
 }
